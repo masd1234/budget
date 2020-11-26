ccd0ed4e14d5abebe3f4e109a9bc83442333a665
Marco Sanchez <masd_123@hotmail.com>
2020-11-26 00:38:18 -0300

    the budget section now has the ffeature to add new budgets and delete the dom element that you chose

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let inputCont = {
   inpt: document.querySelector("#numbers"),
 
-  checkValue() {
+  checkValue(e) {
     let inputValue = this.inpt.value;
     if (inputValue.length === 0) {
       alert("The input field canno be empty");
-      stopPropagation();
+      stopPropagation(e);
     } else {
       return inputValue;
     }
@@ -14,32 +14,46 @@ let inputCont = {
   addValueBudget() {
     let valueChecked = this.checkValue();
     //reads the value of the input field
-    let containerSelector = document.getElementById("budget");
+    let containerSelector = document.querySelector(".detailContainer");
+    let currencySelector = document.getElementById("currency").value;
+    let inputSelector = document.getElementById("numbers");
     // creates the DOM elements
     let containerCreator = document.createElement("div");
     let valueCreator = document.createElement("h4");
+    let currencyCreator = document.createElement("h4");
+    let buttomMinusCreator = document.createElement("buttom");
     //assing the typed value to the DOM node
+    buttomMinusCreator.innerHTML = "x";
     valueCreator.innerHTML = valueChecked;
+    currencyCreator.innerHTML = currencySelector;
     //add the class .cont fot styling
+    buttomMinusCreator.id = "minus";
     containerCreator.classList.add("cont");
+
     // Appends the DOM elements
+    containerCreator.appendChild(currencyCreator);
     containerCreator.appendChild(valueCreator);
+    containerCreator.appendChild(buttomMinusCreator);
     containerSelector.appendChild(containerCreator);
+
+    inputSelector.value = null;
   },
 
-  addValueIncome() {
-    let valueChecked = this.checkValue();
-    //reads the value of the input field
-    let containerSelector = document.getElementById("budget");
-    // creates the DOM elements
-    let containerCreator = document.createElement("div");
-    let valueCreator = document.createElement("h4");
-    //assing the typed value to the DOM node
-    valueCreator.innerHTML = valueChecked;
-    //add the class .cont fot styling
-    containerCreator.classList.add("cont");
-    // Appends the DOM elements
-    containerCreator.appendChild(valueCreator);
-    containerSelector.appendChild(containerCreator);
+  removeContBudget(e) {
+    if (e.target && e.target.id == "minus") {
+      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    }
   },
 };
+
+document.body.addEventListener("click", inputCont.removeContBudget);
+
+/*let values = {
+  valueVariable: 119,
+  valueDom: document.querySelector(".updatedValueBudget"),
+  updater() {
+    console.log('hola')
+    console.log(this.valueVariable);
+    return this.valueVariable++
+  },
+};*/
