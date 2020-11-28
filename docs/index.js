069e79e2322ae7ee983e3cc94f0e4f52f497a0a6
Marco Sanchez <masd_123@hotmail.com>
2020-11-28 18:51:00 -0300

    income section finished, you can type different values with references and elminate indivuals incomes

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,6 @@
 let inputCont = {
   inpt: document.querySelector("#numbers"),
+  inptIcome: document.querySelector("#incomeIput"),
 
   checkValue(e) {
     let inputValue = this.inpt.value;
@@ -48,12 +49,51 @@ let inputCont = {
 
 document.body.addEventListener("click", inputCont.removeContBudget);
 
-/*let values = {
-  valueVariable: 119,
-  valueDom: document.querySelector(".updatedValueBudget"),
-  updater() {
-    console.log('hola')
-    console.log(this.valueVariable);
-    return this.valueVariable++
+let incomeInput = {
+  inputFieldDescription: document.getElementById("incomeInputIdetail"),
+  inputFieldValue: document.getElementById("incomeInputIdValue"),
+  indIcomeCont: document.getElementById("incomeContainer"),
+  currencyIdIncome: document.getElementById("currencyIncome"),
+
+  addValueIncome() {
+    let descriptionValue = this.inputFieldDescription.value;
+    let incomeValue = this.inputFieldValue.value;
+    let currencyIncomeValue = this.currencyIdIncome.value;
+
+    let containerCreatorIncome = document.createElement("div");
+    let valueCreatorIncome = document.createElement("h4");
+    let descriptionCreatorIncome = document.createElement("h4");
+    let currencyCreatorIncome = document.createElement("h4");
+    let buttomDeleteIncome = document.createElement("buttom");
+
+    if (document.getElementById("incomeInputIdValue").value === "" || document.getElementById("incomeInputIdetail").value === "" ) {
+      alert("The input field canno be empty, please fill all the inputs");
+      stopPropagation(e);
+    } else {
+      containerCreatorIncome.id = "incomeCont";
+      buttomDeleteIncome.id = "buttomDeleteId";
+
+      buttomDeleteIncome.innerHTML = "x";
+      descriptionCreatorIncome.innerHTML = descriptionValue;
+      currencyCreatorIncome.innerHTML = currencyIncomeValue;
+      valueCreatorIncome.innerHTML = incomeValue;
+
+      containerCreatorIncome.appendChild(descriptionCreatorIncome);
+      containerCreatorIncome.appendChild(currencyCreatorIncome);
+      containerCreatorIncome.appendChild(valueCreatorIncome);
+      containerCreatorIncome.appendChild(buttomDeleteIncome);
+      this.indIcomeCont.appendChild(containerCreatorIncome);
+
+      this.inputFieldDescription.value = null;
+      this.inputFieldValue.value = null;
+    }
   },
-};*/
+
+  removeContIncome(e) {
+    if (e.target && e.target.id == "buttomDeleteId") {
+      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    }
+  },
+};
+
+document.body.addEventListener("click", incomeInput.removeContIncome);
