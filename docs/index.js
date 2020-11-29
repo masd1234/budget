8bd56c5accf5e48e55476ddbbcb4bfd2f70f6b97
Marco Sanchez <masd_123@hotmail.com>
2020-11-28 22:31:55 -0300

    expenses section finished

--- docs/index.js
+++ docs/index.js
@@ -4,7 +4,10 @@ let inputCont = {
 
   checkValue(e) {
     let inputValue = this.inpt.value;
-    if (inputValue.length === 0) {
+    if (inputValue < 0) {
+      alert("Negative values are not allowed");
+      stopPropagation(e);
+    } else if (inputValue.length === 0) {
       alert("The input field canno be empty");
       stopPropagation(e);
     } else {
@@ -14,7 +17,7 @@ let inputCont = {
 
   addValueBudget() {
     let valueChecked = this.checkValue();
-    //reads the value of the input field
+    //read the value of the input field
     let containerSelector = document.querySelector(".detailContainer");
     let currencySelector = document.getElementById("currency").value;
     let inputSelector = document.getElementById("numbers");
@@ -66,7 +69,13 @@ let incomeInput = {
     let currencyCreatorIncome = document.createElement("h4");
     let buttomDeleteIncome = document.createElement("buttom");
 
-    if (document.getElementById("incomeInputIdValue").value === "" || document.getElementById("incomeInputIdetail").value === "" ) {
+    if (document.getElementById("incomeInputIdValue").value < 0) {
+      alert("Negative values are not allowed");
+      stopPropagation(e);
+    } else if (
+      document.getElementById("incomeInputIdValue").value === "" ||
+      document.getElementById("incomeInputIdetail").value === ""
+    ) {
       alert("The input field canno be empty, please fill all the inputs");
       stopPropagation(e);
     } else {
@@ -97,3 +106,55 @@ let incomeInput = {
 };
 
 document.body.addEventListener("click", incomeInput.removeContIncome);
+
+let expenseInput = {
+  expenseSelector: document.getElementById("expenseContainer"),
+  expenseCurrencySelector: document.getElementById("currencyExpense"),
+  expenseValueSelector: document.getElementById("expenseValue"),
+  expenseDescriptionSelector: document.getElementById(
+    "expenseInputIdDescription"
+  ),
+  addExpense() {
+    if (this.expenseValueSelector.value < 0) {
+      alert("Negative values are not allowed");
+      stopPropagation(e);
+    } else if (
+      this.expenseValueSelector.value === "" ||
+      this.expenseDescriptionSelector.value === ""
+    ) {
+      alert("The input field canno be empty, please fill all the inputs");
+      stopPropagation(e);
+    }
+    let containerCreatorExpense = document.createElement("div");
+    let valueCreatorExpense = document.createElement("h4");
+    let descriptionCreatorExpense = document.createElement("h4");
+    let currencyCreatorExpense = document.createElement("h4");
+    let buttomDeleteExpense = document.createElement("buttom");
+
+    descriptionCreatorExpense.innerHTML = this.expenseDescriptionSelector.value;
+    currencyCreatorExpense.innerHTML = this.expenseCurrencySelector.value;
+    valueCreatorExpense.innerHTML = this.expenseValueSelector.value;
+
+    containerCreatorExpense.classList.add("containerExpenseId");
+
+    buttomDeleteExpense.innerHTML = "x";
+    buttomDeleteExpense.id = "buttomDeleteIdExpense";
+
+    this.expenseSelector.appendChild(containerCreatorExpense);
+    containerCreatorExpense.appendChild(descriptionCreatorExpense);
+    containerCreatorExpense.appendChild(currencyCreatorExpense);
+    containerCreatorExpense.appendChild(valueCreatorExpense);
+    containerCreatorExpense.appendChild(buttomDeleteExpense);
+
+    this.expenseDescriptionSelector.value = null;
+    this.expenseValueSelector.value = null;
+  },
+
+  removeContExpense(e) {
+    if (e.target && e.target.id == "buttomDeleteIdExpense") {
+      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    }
+  },
+};
+
+document.body.addEventListener("click", expenseInput.removeContExpense);
